05c4f37e40b6c1963b74fc51bc4535c5eb4b1fba
sharanteja2211 <sharanteja2211@gmail.com>
2025-07-07 18:30:22 +0530

    header styling

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,7 +8,7 @@ import { store } from './redux/store';
 import { Provider } from 'react-redux';
 import reportWebVitals from './reportWebVitals';
 import "bootstrap/dist/css/bootstrap.css";
-
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
